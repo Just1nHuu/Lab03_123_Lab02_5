66b34b3ed6e37cd30436536781c020e785cd6d94
Nguyễn Hữu Tài <138273682+Just1nHuu@users.noreply.github.com>
2024-04-28 20:05:19 +0700

    123

---
+++ Lab03_4/Client.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices.JavaScript;
+using System.Security.Authentication;
+using System.Security.Permissions;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.Design.AxImporter;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Lab03_4
+{
+    public partial class Client : Form
+    {
+
+        private List<ViTriGheDat> VeDaDat;
+        private List<cDanhGiaPhim> destination;
+        private List<cPhim> phims;
+        private NetworkStream ns;
+        private TcpClient client;
+        public Client()
+        {
+            InitializeComponent();
+            client = new TcpClient();
+            
+        }
+
+        //Hàm thực hiện kết nối đến server
+        private void Client_Load(object sender, EventArgs e)
+        {
+        }
+
+        //Thay đổi lựa chọn tên phim
+        private void ChonPhim_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Thay đổi lựa chọn phong chiếu
+        private void ChonPhong_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+            client.Connect(iPEndPoint);
+        }
+    }
+}
---
+++ Lab03_4/Server.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Text.Json;
+using System.Security.Claims;
+using System.Web;
+namespace Lab03_4
+{
+    //Thực hiện
+    public partial class Server : Form
+    {
+        private Socket socketClient;
+        private List<ViTriGheDat> VeDaDat;
+        private List<cDanhGiaPhim> destination;
+        private List<cPhim> phims;
+        public Server()
+        {
+            InitializeComponent();
+            listView1.Scrollable = true;
+            listView1.View = View.Details;
+            listView1.Columns.Add("Message", 1000);
+
+
+            phims = new List<cPhim>();
+            destination = new List<cDanhGiaPhim>();
+            VeDaDat = new List<ViTriGheDat>();
+            socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //Đọc dữ liệu từ file Input5.JSON
+            string jsonPhim = System.IO.File.ReadAllText("Input5.JSON");
+            phims = JsonSerializer.Deserialize<List<cPhim>>(jsonPhim);
+
+            //Đọc dữ liệu từ file cDanhDanhGia.JSON
+            string jsonDanhGia = System.IO.File.ReadAllText("Output5.JSON");
+            destination = JsonSerializer.Deserialize<List<cDanhGiaPhim>>(jsonDanhGia);
+            //Đọc dữ liệu từ file KiemTraVe.JSON
+            string jsonVeDaDat = System.IO.File.ReadAllText("KiemTraVe.JSON");
+            VeDaDat = JsonSerializer.Deserialize<List<ViTriGheDat>>(jsonVeDaDat);
+        }
+        //Hàm thực lắng nghe kết nối từ client
+        public void StartUnsafeThread()
+        {
+            int byteReceived = 0;
+            // Khởi tạo mảng byte để nhận dữ liệu
+            byte[] received = new byte[1];
+
+            //Tạo socket bên gởi
+            Socket ClientSocket;
+
+            //Tạo socket lắng nghe
+            Socket ListenerSocket = new Socket(
+                //Trả  về họ địa chỉ của địa chỉ hiện hành
+                //ở đây là địa chỉ IPv4 nên chọn AddressFamily.InterNetwork
+                AddressFamily.InterNetwork,
+
+                //Kiểu socket là stream,để nhận dữ liệu liên tục
+                SocketType.Stream,
+
+                //Giao thức truyền dữ liệu là TCP
+                ProtocolType.Tcp);
+
+            //Gán socket với địa chỉ IP và cổng
+            IPEndPoint ipServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+
+            //Gán Socket địa chỉ và port phải lắng nghe vào máy chủ
+            ListenerSocket.Bind(ipServer);
+
+            //Bắt đầu lắng nghe
+            ListenerSocket.Listen(-1);
+
+            //Chấp nhận kết nối từ client
+            ClientSocket = ListenerSocket.Accept();
+
+
+            //Nhận dữ liệu từ client
+            listView1.Items.Add("Server is running on 127.0.0.1:8080");
+            listView1.Items.Add("Server is listening...");
+            while (ClientSocket.Connected)
+            {
+                string text = "";
+                //Nhận dữ liệu từ client đến dấu xuống hàng
+                do
+                {
+                    byteReceived = ClientSocket.Receive(received);
+                    text += Encoding.UTF8.GetString(received);
+                } while (text[text.Length - 1] != '\n');
+                text = "From client: " + text;
+                listView1.Items.Add(new ListViewItem(text));
+            }
+        }
+
+
+        private void Server_Load(object sender, EventArgs e)
+        {
+
+            //Kết nối đến client
+            CheckForIllegalCrossThreadCalls = false;
+            Thread TCPServer = new Thread(new ThreadStart(StartUnsafeThread));
+            TCPServer.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Thoát chương trình
+            System.Environment.Exit(0);
+        }
+    }
+}
